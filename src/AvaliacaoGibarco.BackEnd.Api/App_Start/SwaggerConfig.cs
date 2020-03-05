e45c69e183c20993bae67bd59d4df8430d1496a2
Adilson <adilsondc1@gmail.com>
2020-03-05 10:51:20 -0300

    configurando o swagger

--- src/AvaliacaoGibarco.BackEnd.Api/App_Start/SwaggerConfig.cs
+++ src/AvaliacaoGibarco.BackEnd.Api/App_Start/SwaggerConfig.cs
@@ -2,6 +2,7 @@ using System.Web.Http;
 using Swashbuckle.Application;
 using System.Reflection;
 using System;
+using System.Linq;
 
 namespace AvaliacaoGibarco.BackEnd.Api
 {
@@ -15,17 +16,19 @@ namespace AvaliacaoGibarco.BackEnd.Api
             config
                 .EnableSwagger(c =>
                 {
-                    c.SingleApiVersion($"v1", $"Documenta��o API Gest�o de Contatos {versao}");
+                    c.SingleApiVersion($"v1", $"Documentação API Gestão de Clientes {versao}");
 
                     c.IncludeXmlComments(string.Format(
                     @"{0}\bin\AvaliacaoGibarco.BackEnd.Api.xml",
                     AppDomain.CurrentDomain.BaseDirectory));
 
                     c.DescribeAllEnumsAsStrings();
+                    c.ResolveConflictingActions(x => x.First());
+                    c.UseFullTypeNameInSchemaIds();
                 })
                 .EnableSwaggerUi(c =>
                 {
-                    c.DocumentTitle($"Documenta��o API Gest�o de Contatos {versao}");
+                    c.DocumentTitle($"Documentação API Gestão de Clientes {versao}");
 
                     c.SupportedSubmitMethods(new string[] { });
                 });
